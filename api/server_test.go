959a074ee482006e75df82fd6c16725d7f4f2d7c
Dan Carley <dan.carley@gmail.com>
2018-05-11 14:16:58 +0100

    api: Server for handlers
    Attach all of the handlers that we previously created. The tests are
    minimal because all of the functionality is tested directly in the
    handlers.
    
    Everything is behind basic auth, so that only ourselves and paas-admin
    have access when this is deployed as an app on PaaS.
    
    We've cribbed the code from paas-billing to handle a graceful shutdown
    when CF issues a SIGTERM to stop the app.

---
+++ api/server_test.go
@@ -0,0 +1,129 @@
+package api_test
+
+import (
+	"context"
+	"io"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/labstack/echo"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+
+	. "github.com/alphagov/paas-accounts/api"
+	"github.com/alphagov/paas-accounts/database"
+)
+
+var _ = Describe("Server", func() {
+
+	var (
+		shutdownServer        context.CancelFunc
+		db                    *database.DB
+		tempDB                *database.TempDB
+		server                *echo.Echo
+		addr                  string
+		cleanShutdownComplete chan struct{}
+		basicUsername         = "jeff"
+		basicPassword         = "jefferson"
+	)
+
+	BeforeEach(func() {
+		var err error
+		tempDB, err = database.NewTempDB()
+		Expect(err).ToNot(HaveOccurred())
+
+		db, err = database.NewDB(tempDB.TempConnectionString)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(db.Init()).To(Succeed())
+
+		var ctx context.Context
+		ctx, shutdownServer = context.WithCancel(context.Background())
+
+		server = NewServer(Config{
+			DB:                db,
+			BasicAuthUsername: basicUsername,
+			BasicAuthPassword: basicPassword,
+			LogWriter:         GinkgoWriter,
+		})
+		cleanShutdownComplete = make(chan struct{})
+		go func() {
+			Expect(ListenAndServe(ctx, server, "0.0.0.0:0")).To(Succeed())
+			close(cleanShutdownComplete)
+		}()
+		Eventually(func() net.Listener {
+			return server.Listener
+		}).ShouldNot(BeNil())
+		addr = server.Listener.Addr().String()
+	})
+
+	AfterEach(func() {
+		shutdownServer()
+		Eventually(cleanShutdownComplete, 10*time.Second).Should(BeClosed())
+		db.Close()
+		Expect(tempDB.Close()).To(Succeed())
+	})
+
+	DescribeTable("should expose status route to public without basic auth",
+		func(method, path string) {
+			var body io.Reader
+			if method == http.MethodPost {
+				body = strings.NewReader("{}")
+			}
+			url := "http://" + addr + path
+			req, err := http.NewRequest(method, url, body)
+			Expect(err).ToNot(HaveOccurred())
+			client := &http.Client{}
+			res, err := client.Do(req)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(res.StatusCode).To(Equal(200))
+		},
+		Entry("GET /", "GET", "/"),
+	)
+
+	DescribeTable("should not expose routes to public",
+		func(method, path string) {
+			var body io.Reader
+			if method == http.MethodPost {
+				body = strings.NewReader("{}")
+			}
+			url := "http://" + addr + path
+			req, err := http.NewRequest(method, url, body)
+			Expect(err).ToNot(HaveOccurred())
+			client := &http.Client{}
+			res, err := client.Do(req)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(res.StatusCode).To(Equal(401))
+		},
+		Entry("POST /agreements", "POST", "/agreements"),
+		Entry("PUT /documents/:name", "PUT", "/documents/doc-one"),
+		Entry("GET /documents/:name", "GET", "/documents/doc-one"),
+		Entry("GET /users/569a91c6-7f5d-4dac-82a2-db85cc595c75/documents", "GET", "/users/"),
+	)
+
+	DescribeTable("should allow access with basic auth credentials",
+		func(method, path string, responseCode int) {
+			var body io.Reader
+			if method != http.MethodGet {
+				body = strings.NewReader("{}")
+			}
+			url := "http://" + addr + path
+			req, err := http.NewRequest(method, url, body)
+			Expect(err).ToNot(HaveOccurred())
+			req.Header.Set("Content-Type", echo.MIMEApplicationJSON)
+			req.SetBasicAuth(basicUsername, basicPassword)
+			client := &http.Client{}
+			res, err := client.Do(req)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(res.StatusCode).To(Equal(responseCode))
+		},
+		Entry("POST /agreements", "POST", "/agreements", 500),
+		Entry("PUT /documents/:name", "PUT", "/documents/doc-one", 500),
+		Entry("GET /documents/:name", "GET", "/documents/doc-one", 404),
+		Entry("GET /users/:uuid/documents", "GET", "/users/569a91c6-7f5d-4dac-82a2-db85cc595c75/documents", 200),
+	)
+
+})
